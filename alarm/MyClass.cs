d234c8b0888cd55cf80da1aaa57457f6729e9909
akaishi takeshi <t_akaishi@cgate.jp>
2016-05-25 23:33:32 +0900

    init

---
+++ alarm/MyClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace alarm
+{
+	public class MyClass : Realms.RealmObject
+	{
+		[ObjectId]
+		public Int64 Id { get; set; }
+		// [Indexed]
+		public string Name { get; set; }
+
+		public static void insert()
+		{
+			var realm = Realms.Realm.GetInstance ();
+			realm.Write (() => {
+				var obj = realm.CreateObject<MyClass>();
+				obj.Id = 0;
+				obj.Name = "todo";
+			});
+		}
+
+		public static MyClass findById(Int64 id)
+		{
+			var realm = Realms.Realm.GetInstance ();
+
+
+			return realm.All<MyClass>().Where(d => d.Id == id).First();
+
+		}
+
+		public static void init()
+		{
+			
+		}
+	}
+}
+
