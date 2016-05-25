472faecd5eca60cf5c855a87a79af259a4902f45
akaishi takeshi <t_akaishi@cgate.jp>
2016-05-25 23:51:04 +0900

    rss model

---
+++ alarm/Rss.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace alarm
+{
+	public class Rss : Realms.RealmObject
+	{
+		[Realms.ObjectId]
+		public string Key { get; set; }
+		public string Url { get; set; }
+
+
+
+		static void init()
+		{
+			var realm = Realms.Realm.GetInstance ();
+
+			using (var transaction = realm.BeginWrite ()) {
+				var news = realm.CreateObject<Rss> ();
+				news.Key = "news";
+				news.Url = "http://todo";
+
+				var train = realm.CreateObject<Rss> ();
+				train.Key = "train";
+				train.Url = "http://todo";
+
+				transaction.Commit ();
+			}
+		}
+	}
+}
+
